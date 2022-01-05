8332e2399c62f64928bad0c57c416b9bc5a29f21
Ljubo Nikolic <ljubonikolic@gmail.com>
2022-01-05 22:48:31 +0100

    revert - setup env first

--- __hacker-rank/00-problem-solving-begginer/01-password-decrypt/src/app.ts
+++ __hacker-rank/00-problem-solving-begginer/01-password-decrypt/src/app.ts
@@ -1,80 +1,68 @@
-function decryptPassword(s: string): string {
-    let result: string = ""
-    let original: string = ""
-    let arr: string[] = s.split('')
-
-
-    arr.forEach(x => {
-        original += x
+function convertArrToString(array: string[]): string {
+    let string: string = ""
+    array.forEach(x => {
+        string += x
     })
-    console.log("Before:\t\t" + original);
-
-    // 43Ah*ck0rr0nk => hAck3rr4nk
-
-    let i: number = arr.length - 1
-    let tmpIndex: number = arr.length - 1
-    while (i <= arr.length -1 && i != 0) {
-        arr.forEach(s => {
-
-            if (!isNaN(+s) && +s == 0) {
-                arr[0] = s
-                arr.splice(arr.indexOf("0"), -1)
-
-                let tmpArr = ""
-                arr.forEach(s => {
-                    tmpArr += s
-                })
-                console.log("===> Number: " + i + " - " + s + " lastIndex: " + tmpIndex + " arr: " + tmpArr);
-            }
-            else if (
-                (s == s.toUpperCase()) &&
-                (arr[i + 1] == arr[i + 1].toLowerCase()) &&
-                (arr[i + 2] == "*")) {
-                let tmp = s
-                arr[i] == arr[i + 1]
-                arr[i + 1] == tmp
-                arr.splice(i + 2, 1)
-                i += 1
-
-                let tmpArr = ""
-                arr.forEach(s => {
-                    tmpArr += s
-                })
-                console.log("===> Number: " + i + " - " + s + " arr: " + tmpArr);
-            } else {
-                i += 1
-
-                let tmpArr = ""
-                arr.forEach(s => {
-                    tmpArr += s
-                })
-                console.log("===> Number: " + i + " - " + s + " arr: " + tmpArr);
-            }
-        })
-    }
-
-    // Convert to string
-    arr.reverse().forEach(s => {
-        result += s
-    })
-    return "After:\t\t" + result
+    return string
 }
+
+function convertStringToArray(s: string): string[] {
+    let result: string[] = s.split('')
+    return result
+}
+
+function isCharNumeric(s: string) {
+    return !isNaN(parseInt(s));
+}
+
+function deleteCharInString(s: string, n: number = 0): string {
+    let result: string[] = convertStringToArray(s); // 43Ah*ck0rr0nk
+    result.splice(n, 1)
+    return convertArrToString(result) // 43Ah*ck0rr0nk
+}
+
+function swapChars(s: string, old: number, goal: number): string {
+    let result: string[] = convertStringToArray(s);
+    result[goal] = s[old]
+
+    return convertArrToString(result)
+}
+
+function decryptPassword(s: string): string {
+    // let arr = convertStringToArray(s);
+
+    // let i = 0
+    // while (i < arr.length - 1) {
+    //     if (!isNaN(+arr[i]) && +arr[i] == 0) {
+    //         arr[i] = arr[0] // copy first number value to position of 0
+    //         arr[0] = ''  // remove first number
+    //     } else if (
+    //         (arr[i] == arr[i].toUpperCase()) &&
+    //         (arr[i + 1] == arr[i + 1].toLowerCase()) &&
+    //         (arr[i + 2] == '*'))
+    //     {
+    //         arr[i + 2] = '' // remove *
+    //         let tmp = arr[i]
+    //         arr[i] = arr[i + 1]
+    //         arr[i + 1] = tmp
+    //     } else {
+    //         i += 1
+    //     }
+    // }
+    let tmp = s
+
+    return tmp
+}
+
+let hacker: string = "43Ah*ck0rr0nk" // hAck3rr4nk
+let dummyString = 'Javascript is popular language'
+console.log(decryptPassword(hacker));
+console.log("Goal:\t\thAck3rr4nk");
 
 // let test1: string = "51Pa*0Lp*0e"// aP1pL5e
 // console.log(decryptPassword(test1));
 // console.log("Goal:\taP1pL5e");
 
-let hacker: string = "43Ah*ck0rr0nk" // hAck3rr4nk
-console.log(decryptPassword(hacker));
-console.log("Goal:\t\thAck3rr4nk");
-
 // let test2 = "pTo*Ta*O" // poTaTO
 // console.log("poTaTO")
 // console.log(decryptPassword(test2))
-
-
-// Convert to string
-// let test: string[] = hacker.split('')
-// console.log(test.length);
-// console.log(test.slice(1));
-
