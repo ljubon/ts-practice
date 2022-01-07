a4fe0b5f80ab8c9ea6793a72338af7f83cb4660e
Ljubo Nikolic <ljubonikolic@gmail.com>
2022-01-07 11:13:45 +0100

    SOLVED decrypt password

--- hackerrank/00-problem-solving-begginer/01-password-decrypt/src/app.ts
+++ hackerrank/00-problem-solving-begginer/01-password-decrypt/src/app.ts
@@ -11,55 +11,34 @@ function convertStringToArray(s: string): string[] {
     return result
 }
 
-function isCharNumeric(s: string) {
-    return !isNaN(parseInt(s));
-}
-
-function deleteCharInString(s: string, n: number = 0): string {
-    let result: string[] = convertStringToArray(s); // 43Ah*ck0rr0nk
-    result.splice(n, 1)
-    return convertArrToString(result) // 43Ah*ck0rr0nk
-}
-
-function swapChars(s: string, old: number, goal: number): string {
-    let result: string[] = convertStringToArray(s);
-    result[goal] = s[old]
-
-    return convertArrToString(result)
-}
-
 function decryptPassword(s: string): string {
     let arr = convertStringToArray(s);
-
-    // let i = 0
-    // while (i < arr.length - 1) {
-    //     if (!isNaN(+arr[i]) && +arr[i] == 0) {
-    //         arr[i] = arr[0] // copy first number value to position of 0
-    //         arr[0] = ''  // remove first number
-    //     } else if (
-    //         (arr[i] == arr[i].toUpperCase()) &&
-    //         (arr[i + 1] == arr[i + 1].toLowerCase()) &&
-    //         (arr[i + 2] == '*'))
-    //     {
-    //         arr[i + 2] = '' // remove *
-    //         let tmp = arr[i]
-    //         arr[i] = arr[i + 1]
-    //         arr[i + 1] = tmp
-    //     } else {
-    //         i += 1
-    //     }
-    // }
-
-    return convertArrToString(arr);
     
+    let i = 0
+    while (i < arr.length - 1) {
+        if (+arr[i] === 0 ) {
+            arr[arr.lastIndexOf('0')] = arr[0]
+            arr.splice(0, 1)
+        } else if (
+            (arr[i] == arr[i].toUpperCase()) &&
+            (arr[i + 1] == arr[i + 1].toLowerCase()) &&
+            (arr[i + 2] == '*')
+        ) {
+            arr[i + 2] = ''
+            let tmp = arr[i]
+            arr[i] = arr[i + 1]
+            arr[i + 1] = tmp
+        }
+        i += 1
+    }
+    return convertArrToString(arr);
 }
 
 let encrypted: string = "43Ah*ck0rr0nk"
-let decrypted: string =  "hAck3rr4nk"
+let decrypted: string = "hAck3rr4nk"
 
 console.log("Encrypted:\t\t" + encrypted);
 console.log("Decrypted:\t\t" + decryptPassword(encrypted));
 console.log("Expected:\t\t" + decrypted);
-( decrypted == decryptPassword(encrypted)) ? console.log("It works!"): console.log("Not yet...");
-
+(decrypted == decryptPassword(encrypted)) ? console.log("It works!") : console.log("Not yet...");
 
