9e91ebb74587058fd7763f20f53dcc7268f18cc9
Ljubo Nikolic <ljubonikolic@gmail.com>
2022-01-07 12:59:16 +0100

    WIP -min cost

---
+++ hackerrank/00-problem-solving-begginer/02-min-cost/src/app.ts
@@ -0,0 +1,13 @@
+// function getMinCost(crewId: number[], jobId: number[]): number {
+//     let result: number = 0
+    
+//     return result
+// }
+
+let input = [4, 5, 1, 4, 2, 4, 4, 7, 9, 10]
+let expected = 18
+console.log("Input:\t\t" + input);
+// console.log("Decrypted:\t\t" + getMinCost(input));
+console.log("Expected:\t\t" + expected);
+// (input == getMinCost(input)) ? console.log("It works!") : console.log("Not yet...");
+
